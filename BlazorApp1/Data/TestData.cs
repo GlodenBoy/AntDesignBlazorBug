6b67bdc0fda6e171bb859fcee1a122c9ac0adae4
Ninety <luoxiaoji@qq.com>
2022-10-27 10:54:45 +0800

    modify

---
+++ BlazorApp1/Data/TestData.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp1.Data
+{
+    public class TestData
+    {
+
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public string Tips { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public List<TestData> Children { get; set; }
+    }
+}
